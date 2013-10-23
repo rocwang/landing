3354b29e648a2a426bff0f8d0ef7bf246e78d955
Peng Wang <rocinwinter@gmail.com>
2013-10-24 00:50:19 +0800

    Add global sliding effect.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+jQuery(function($) {
+    $('.roc').click(function(e) {
+        if ($('.main').hasClass('show-roc')) {
+            $('.main').removeClass('show-roc');
+        } else {
+            $('.main').removeClass('show-vivien').addClass('show-roc');
+        }
+    });
 
+    $('.vivien').click(function(e) {
+        if ($('.main').hasClass('show-vivien')) {
+            $('.main').removeClass('show-vivien');
+        } else {
+            $('.main').removeClass('show-roc').addClass('show-vivien');
+        }
+    });
+});
