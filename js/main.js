0cc8e45e2cef115ebb52e25641feb69807989724
Peng Wang <rocinwinter@gmail.com>
2013-10-24 21:12:05 +0800

    Improve animation.
    Add background.
    Use a new animation.
    Add Youtube code.
    Add QR Code demo.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,43 @@
 jQuery(function($) {
-    $('.roc').click(function(e) {
-        if ($('.main').hasClass('show-roc')) {
-            $('.main').removeClass('show-roc');
-        } else {
-            $('.main').removeClass('show-vivien').addClass('show-roc');
-        }
+    var bg_width = 2000;
+
+    $('.roc-name').click(function(e) {
+        if ($('body').hasClass('show-both')) {
+
+            $('body').removeClass().addClass('both-roc show-roc').css('background-position-x', function(index, value) {
+                return $('body').width() - bg_width/2;
+            });
+
+        } else if ($('body').hasClass('show-roc')) {
+
+            $('body').removeClass().addClass('roc-both show-both').css('background-position-x', 'center');
+
+        } else if ($('body').hasClass('show-vivien')) {
+
+            $('body').removeClass().addClass('vivien-roc show-roc').css('background-position-x', function(index, value) {
+                return $('body').width() - bg_width/2;
+            });
+
+        };
     });
 
-    $('.vivien').click(function(e) {
-        if ($('.main').hasClass('show-vivien')) {
-            $('.main').removeClass('show-vivien');
-        } else {
-            $('.main').removeClass('show-roc').addClass('show-vivien');
-        }
+    $('.vivien-name').click(function(e) {
+        if ($('body').hasClass('show-both')) {
+
+            $('body').removeClass().addClass('both-vivien show-vivien').css('background-position-x', function(index, value) {
+                return -bg_width/2;
+            });
+
+        } else if ($('body').hasClass('show-vivien')) {
+
+            $('body').removeClass().addClass('vivien-both show-both').css('background-position-x', 'center');
+
+        } else if ($('body').hasClass('show-roc')) {
+
+            $('body').removeClass().addClass('roc-vivien show-vivien').css('background-position-x', function(index, value) {
+                return -bg_width/2;
+            });
+
+        };
     });
 });
