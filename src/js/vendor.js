e50dcaca12eea76debb8139c9f048f6d791db235
Roc Wang <me@kiwiberry.nz>
2015-09-06 12:30:07 +1200

    Add svg4everybody.

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,6 +1,8 @@
 module.exports = [
-  '../..//node_modules/photoswipe/dist/photoswipe.js',
-  '../..//node_modules/photoswipe/dist/photoswipe-ui-default.js',
+  '../../node_modules/photoswipe/dist/photoswipe.js',
+  '../../node_modules/photoswipe/dist/photoswipe-ui-default.js',
+  '../../node_modules/svg4everybody/dist/svg4everybody.js',
+  'vendor/svg4everybody.js',
   'vendor/google-analytics.js',
   'vendor/zopim.js',
 ];
