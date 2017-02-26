bccbe458e5da2f95bc7f257a83ee3910205ac5cf
Roc Wang <roc@kiwiberry.nz>
2017-02-26 16:40:46 +1300

    Add www.rocwang.me to purge-cloudflare.sh

--- purge-cloudflare.sh
+++ purge-cloudflare.sh
@@ -5,7 +5,7 @@ curl -X DELETE \
     -H "X-Auth-Email: rocinwinter@gmail.com" \
     -H "X-Auth-Key: $(<cloudflare-api-key)" \
     -H "Content-Type: application/json" \
-    --data '{"files":["https://rocwang.me/", "https://rocwang.me/index.html"]}' \
+    --data '{"files":["https://rocwang.me/", "https://www.rocwang.me/", "https://rocwang.me/index.html", "https://www.rocwang.me/index.html"]}' \
     "https://api.cloudflare.com/client/v4/zones/48a5ff753ce70f5261c0762c6bd03c34/purge_cache"
 
 open https://www.rocwang.me/
