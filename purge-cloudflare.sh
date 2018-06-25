1fa3ecedab2aa202c5203179f2aec590546a598e
Roc Wang <roc@kiwiberry.nz>
2018-06-25 23:22:37 +1200

    Update zone ID in purge-cloudflare.sh

--- purge-cloudflare.sh
+++ purge-cloudflare.sh
@@ -6,6 +6,6 @@ curl -X DELETE \
     -H "X-Auth-Key: $(<cloudflare-api-key)" \
     -H "Content-Type: application/json" \
     --data '{"files":["https://kiwiberry.nz/", "https://www.kiwiberry.nz/", "https://kiwiberry.nz/index.html", "https://www.kiwiberry.nz/index.html"]}' \
-    "https://api.cloudflare.com/client/v4/zones/48a5ff753ce70f5261c0762c6bd03c34/purge_cache"
+    "https://api.cloudflare.com/client/v4/zones/e2ef99d8481d672b6f80ff8b3577ab4a/purge_cache"
 
 open https://kiwiberry.nz/
