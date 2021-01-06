7ffde8a433040e4bda34896244e92504f6f578eb
Roc Wang <roc@kiwiberry.nz>
2021-01-07 11:20:40 +1300

    chore: enable https on dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { homedir } from "os";
+import { readFileSync } from "fs";
 
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  server: {
+    open: true,
+    https: {
+      key: readFileSync(homedir() + "/.localhost_ssl/server.key"),
+      cert: readFileSync(homedir() + "/.localhost_ssl/server.crt"),
+    },
+  },
+  optimizeDeps: { exclude: ["prettier"] },
+});
